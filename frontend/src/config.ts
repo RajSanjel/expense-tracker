364ca4034b45286dd5d42bb9766af3b2f71b26e4
Raj Sanjel <rajsanjel201@gmail.com>
2024-06-03 15:08:23 +0545

    Bug fixes

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    // "API_BASE_URL": "https://expense-tracker-xylt.vercel.app",
-    "API_BASE_URL": "http://localhost:5000"
+    "API_BASE_URL": "https://expense-tracker-xylt.vercel.app",
+    // "API_BASE_URL": "http://localhost:5000"
 }
 export default config;
