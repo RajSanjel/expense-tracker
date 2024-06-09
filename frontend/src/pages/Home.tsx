91323ff32a539b01e39654d5a296c3142e8a4982
Raj Sanjel <rajsanjel201@gmail.com>
2024-06-09 14:12:41 +0545

    UI completed

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -14,35 +14,42 @@ const features = [{
 }, {
     title: "Chart",
     body: "Analyze your weekly trends and performance with insightful charts.",
-    img: "https://media.istockphoto.com/id/1503735047/photo/data-scientists-analyze-and-visualize-massive-data-on-virtual-screens-using-ai-to-process.jpg?s=1024x1024&w=is&k=20&c=a9mEe-bje9bczIQ1LBY410MlGSdEaRoRj6ULRO7rYMg="
+    img: "https://img.freepik.com/premium-vector/bar-chart-with-line-graph-show-effectiveness-business-earning-planning-management_120819-711.jpg?w=900"
 },
 {
-    title: "Activity",
+    title: "Manage",
     body: "Efficiently track and manage your daily income and expenses.",
-    img: "https://media.istockphoto.com/id/1503735047/photo/data-scientists-analyze-and-visualize-massive-data-on-virtual-screens-using-ai-to-process.jpg?s=1024x1024&w=is&k=20&c=a9mEe-bje9bczIQ1LBY410MlGSdEaRoRj6ULRO7rYMg="
+    img: "https://i.pinimg.com/564x/36/27/af/3627af29a4e443d8eeb98f01aa69d081.jpg"
 }]
 
 
 export function Home() {
-    document.title = "Earning Tracker"
+    document.title = "Expense Tracker"
     return (
         <>
-            <header className="h-96 bg-white w-5/6 p-10 text-left shadow-md rounded-md grid content-center mb-10">
-                <div className="w-2/6">
-                    <p className="text-3xl font-bold w-4/5">
-                        Welcome to Earning Tracker
+            <header className="h-96 bg-white w-full md:w-5/6 p-10 text-left 
+            shadow-lg rounded-xl grid content-center mb-10
+             grid-flow-col 
+             bg-[url('https://cdn.dribbble.com/users/43762/screenshots/1193020/media/18e355ce88914a10ff30668836b1b997.gif')] 
+             bg-no-repeat bg-center lg:bg-right
+             bg-cover
+             xl:bg-[length:730px]
+             ">
+                <div >
+                    <p className="text-3xl font-bold text-white lg:text-gray-950">
+                        Welcome to<br />Expense Tracker
                     </p>
-                    <span>
-                        <Button className="my-4 mr-2">
+                    <span className="grid gap-2  grid-flow-col w-10 mt-4">
+                        <Button className="bg-white text-black hover:bg-slate-100 xl:bg-slate-950 xl:text-white xl:hover:bg-slate-950">
                             Signup
                         </Button>
-                        <Button>
+                        <Button variant={"outline"}>
                             Login
                         </Button>
                     </span>
                 </div>
             </header>
-            <p className="text-2xl font-semibold p-2">Features</p>
+            <p className="text-2xl font-semibold p-2 mb-3">Features</p>
             <div className="grid grid-flow-row md:grid-flow-col gap-4 md:container lg:w-4/5">
                 {
                     features.map((feature) => (
@@ -59,14 +66,14 @@ export function Home() {
 function FeatureCards({ title, body, img }: FeatureProps) {
     return (
         <>
-            <Card className="shadow-xl rounded-md mb-10">
-                <CardHeader className="font-semibold text-xl">
+            <Card className="shadow-xl rounded-xl mb-10">
+                <CardHeader className="font-semibold text-xl text-center">
                     {title}
                 </CardHeader>
-                <CardContent>
+                <CardContent className="w-full h-48 overflow-hidden">
                     <img src={img} alt="" className="rounded-md" />
                 </CardContent>
-                <CardFooter className="self-end text-lg font-bolder text-left">
+                <CardFooter className="self-end text-lg font-bolder text-left mt-3">
                     {body}
                 </CardFooter>
             </Card>
